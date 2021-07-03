21619593ad8ef2132d20cf873492e5d4d3084380
Neil Doherty <neil.doherty@snhu.edu>
2021-07-03 11:02:25 -0400

    Created foundation of The Professor bot

---
+++ main.py
@@ -0,0 +1,37 @@
+# import discord api
+import discord
+# imports for token
+from dotenv import load_dotenv
+import os
+
+# load token
+load_dotenv('.env')
+TOKEN = os.getenv("TOKEN")
+client = discord.Client()
+
+# Confirm bot is online
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is online and connected to the server.')
+
+# ****MAIN CODE BODY GOES BELOW HERE****
+
+# Bot greeting test feature
+
+#
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$Hello'):
+        channel = message.channel
+        await channel.send('I AM ALIVE')
+
+
+# ****MAIN CODE BODY GOES ABOVE HERE****
+
+# Connect to the server
+client.run(TOKEN)
