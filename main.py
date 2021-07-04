af56a691c714e4e1078a3f0eca7179d57ea84edb
Austin Grady <austin.grady@snhu.edu>
2021-07-03 21:56:09 -0400

    Started reaction rule agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import discord api
 import discord
+from discord import emoji
 from discord.ext import commands
 # imports for token
 from dotenv import load_dotenv
@@ -18,6 +19,23 @@ async def on_ready():
     print(f'{bot.user} is online and connected to the server.')
 
 # ****MAIN CODE BODY GOES BELOW HERE****
+
+# Bot Initial Rules Agreement
+rules_message_id = 860962384276226078
+@bot.event
+async def on_raw_reaction_add(payload):
+    print(payload)
+
+    if (payload.message_id != rules_message_id):
+        return
+    
+    if (payload.emoji.name == '👍'):
+        print(payload.member.name + " accepted the rules")
+    elif (payload.emoji.name == '👎'):
+        print(payload.member.name + " declined the rules")
+    else:
+        print("Invalid Option")
+
 
 # Bot greeting test feature
 
