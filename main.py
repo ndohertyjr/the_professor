93c80a0125b0952c3d7ccc58d9ea69faeb6f308a
Austin Grady <austin.grady@snhu.edu>
2021-07-03 23:48:42 -0400

    Dynamic Rules Agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # import discord api
 import discord
-from discord import emoji
 from discord.ext import commands
 # imports for token
 from dotenv import load_dotenv
@@ -12,28 +11,32 @@ TOKEN = os.getenv("TOKEN")
 bot = commands.Bot(command_prefix='!')
 
 # Confirm bot is online
-
+rules_channel = None 
+rules_message_id = None
+test_role = None
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is online and connected to the server.')
+    global rules_channel, rules_message_id, test_role
+    rules_channel = await discord.utils.get(bot.get_all_channels(), name='rules')
+    rules_message_history = await rules_channel.history(limit = 1).flatten()
+    rules_message_id = rules_message_history[0].id
+    test_role = await discord.utils.get(bot.get_guild(860919967749505025).roles, name="TestRole")
 
 # ****MAIN CODE BODY GOES BELOW HERE****
 
 # Bot Initial Rules Agreement
-rules_message_id = 860962384276226078
-role = discord.abc.Snowflake
-role.id = 861064435895107585
+
 @bot.event
 async def on_raw_reaction_add(payload):
-    print(payload)
 
     if (payload.message_id != rules_message_id):
         return
     
     if (payload.emoji.name == '👍'):
         print(payload.member.name + " accepted the rules")
-        await payload.member.add_roles(role, reason=None, atomic=True)
+        await payload.member.add_roles(test_role, reason=None, atomic=True)
     elif (payload.emoji.name == '👎'):
         print(payload.member.name + " declined the rules")
     else:
